b1ae2e41fb3a67685e571b4b6719305e1c4818fc
EliteNarNar <77871782+EliteNarNar@users.noreply.github.com>
2023-08-24 21:49:18 +0100

    migrating

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ GUILD_ID = 1123417784886497302
 WELCOME_CHANNEL = 1123423381132423219
 LEAVE_CHANNEL = 1123428819852857345
 TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD_ID =  1123417784886497302
-#GUILD_ID = 819358157569916949
+GUILD_ID = 488627204213833728
 
 bot = interactions.Client(token=TOKEN,intents=interactions.Intents.ALL)
 @bot.event
@@ -20,24 +19,26 @@ async def on_ready():
     global guild
     guild = await get(bot, interactions.Guild, object_id=GUILD_ID)
 
-@bot.event(name="on_guild_member_add")
-async def on_guild_member_add(member):
-    channel = await get(bot, interactions.Channel, object_id=WELCOME_CHANNEL)
-    await channel.send(f"{member.mention} has joined the server!")
+# @bot.event(name="on_guild_member_add")
+# async def on_guild_member_add(member):
+#     channel = await get(bot, interactions.Channel, object_id=WELCOME_CHANNEL)
+#     await channel.send(f"{member.mention} has joined the server!")
 
-@bot.event(name="on_guild_member_remove")
-async def on_guild_member_remove(member):
-    channel = await get(bot, interactions.Channel, object_id=LEAVE_CHANNEL)
+# @bot.event(name="on_guild_member_remove")
+# async def on_guild_member_remove(member):
+#     channel = await get(bot, interactions.Channel, object_id=LEAVE_CHANNEL)
     
-    await channel.send(f"**{member.username}** has left the server.") 
+#     await channel.send(f"**{member.username}** has left the server.") 
 @bot.command(name="sendbuttons", scope=GUILD_ID,)
 async def sendButtons(ctx: interactions.CommandContext):
     channel = ctx.channel
-
+    if ctx.author.id != 339866237922181121: 
+        await ctx.send("You are not allowed to use this command!", ephemeral=True)
+        return
     
     buttons = [interactions.Button(style=interactions.ButtonStyle.SUCCESS, label="Spoons", custom_id="spoons"),
                interactions.Button(style=interactions.ButtonStyle.SUCCESS, label="2 mans ping", custom_id="2"),
-               interactions.Button(style=interactions.ButtonStyle.SUCCESS, label="4 mans ping", custom_id="4m"),
+               interactions.Button(style=interactions.ButtonStyle.SUCCESS, label="4 mans ping", custom_id="4"),
                 interactions.Button(style=interactions.ButtonStyle.SUCCESS, label="6 mans ping", custom_id="6")#
 
     ]
@@ -51,16 +52,16 @@ async def on_component(ctx):
   try:
     print(ctx.custom_id)
     if ctx.custom_id == "4":
-        role = await guild.get_role(1123454868452016178)
+        role = await guild.get_role(902546224622735411)
     
     elif ctx.custom_id == "6":
-        role = await guild.get_role(1123454904334307348)
+        role = await guild.get_role(757363680688996523)
     
     elif ctx.custom_id == "2":
-        role = await guild.get_role(1123454808750293032)
+        role = await guild.get_role(1144367945691189338)
     
     elif ctx.custom_id == "spoons":
-        role = await guild.get_role(1123454958377906226)
+        role = await guild.get_role(636985627375566935)
 
     if role.id in ctx.author.roles:
         await ctx.author.remove_role(role=role, guild_id=GUILD_ID)
